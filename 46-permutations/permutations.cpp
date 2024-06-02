e77378d2165cb1551b16cadc0c81793a91b4aede
Anikait Sehwag <128905481+SundownRises@users.noreply.github.com>
2024-06-02 15:47:57 +0530

    Time: 5 ms (35.21%) | Memory: 8.9 MB (74.63%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -6,9 +6,9 @@ public:
             return;
         }
         for (int i = first; i < nums.size(); ++i) {
-            swap(nums[first], nums[i]);  // Swap to place the i-th element first
-            backtrack(first + 1, nums, result);  // Generate all permutations for the next position
-            swap(nums[first], nums[i]);  // Backtrack
+            swap(nums[first], nums[i]); 
+            backtrack(first + 1, nums, result); 
+            swap(nums[first], nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
