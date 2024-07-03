63a1f47d12716978fd39cfd717dc29d7358e4985
Anikait Sehwag <128905481+SundownRises@users.noreply.github.com>
2024-07-03 21:27:48 +0530

    Time: 44 ms (64.96%) | Memory: 9.3 MB (70.54%) - LeetSync

---
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getMoneyAmount(int n) {
+    vector<std::vector<int>> dp(n + 1, std::vector<int>(n + 1, 0));
+    
+    for (int len = 2; len <= n; ++len) {
+        for (int start = 1; start <= n - len + 1; ++start) {
+            int end = start + len - 1;
+            dp[start][end] = INT_MAX;
+            for (int k = start; k < end; ++k) {
+                int cost = k + max(dp[start][k - 1], dp[k + 1][end]);
+                dp[start][end] = min(dp[start][end], cost);
+            }
+        }
+    }
+    
+    return dp[1][n];
+
+
+    }
+};
