8d568f7736e2e21e09299d99e46cf226d32a2145
Anikait Sehwag <128905481+SundownRises@users.noreply.github.com>
2024-10-23 19:29:52 +0530

    Time: 2 ms (98.52%) | Memory: 10.2 MB (82.12%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+            string result;
+    
+    for (char digit : num) {
+        while (!result.empty() && result.back() > digit && k > 0) {
+            result.pop_back(); 
+            k--; 
+        }
+        result.push_back(digit); 
+    }
+    
+    while (k > 0 && !result.empty()) {
+        result.pop_back();
+        k--;
+    }
+    
+    int start = 0;
+    while (start < result.size() && result[start] == '0') {
+        start++;
+    }
+    
+    if (start == result.size()) {
+        return "0";
+    }
+    
+    return result.substr(start);
+    }
+};
