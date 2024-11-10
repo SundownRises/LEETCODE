5f82814e37016d3617bb7a2ff9db05cdb1e2f696
Anikait Sehwag <128905481+SundownRises@users.noreply.github.com>
2024-11-10 11:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (43.61%) - LeetSync

---
+++ 944-smallest-range-i/smallest-range-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {
+    int minNum = *min_element(nums.begin(), nums.end());
+    int maxNum = *max_element(nums.begin(), nums.end());
+    
+    int minScore = max(0, maxNum - minNum - 2 * k);
+    
+    return minScore;
+    }
+};
