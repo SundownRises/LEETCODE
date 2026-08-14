07487ca4b5fe3db6cd19748dc8261ca424138320
Anikait Sehwag <128905481+SundownRises@users.noreply.github.com>
2026-08-14 08:32:03 +0530

    Time: 4 ms (14.41%) | Memory: 9.9 MB (54.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string cleaned = "";
-
-        for (char c : s) {
-            if (isalnum(c)) {
-                cleaned += tolower(c);
-            }
-        }
-
         int left = 0;
-        int right = cleaned.size() - 1;
+        int right = s.size() - 1;
 
         while (left < right) {
-            if (cleaned[left] != cleaned[right]) {
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
+
             left++;
             right--;
         }
